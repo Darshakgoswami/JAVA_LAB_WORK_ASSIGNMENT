1834c08cb1f83ff6a808ecaca88ce36812b8b37a
Darshak <darshakgoswami122@gmail.com>
2025-07-03 12:44:48 +0530

    added

---
+++ basic/Encapsulation.java
@@ -0,0 +1,26 @@
+package basic;
+public class Encapsulation {
+	private int id;
+	private String name, address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public static void main(String[] args) {
+	
+	}
+}

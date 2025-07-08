7b7767443d89bc2d751e8d9ab70dd7bb44e19737
Darshak <darshakgoswami122@gmail.com>
2025-07-08 12:55:51 +0530

    added

---
+++ basic/Exception_Handling.java
@@ -0,0 +1,26 @@
+package basic;
+
+public class Exception_Handling 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Before Error !");
+		try
+		{
+		int i = 9/0;
+		System.out.println(i);
+		
+		int a[]=new int[5];
+		a[6]=90;
+		
+		String s = null;
+		System.out.println(s.length());
+		}
+		catch(Exception e)
+		{
+			System.out.println("This is Error !!!");
+		}
+		System.out.println("After Error !");
+	}
+}

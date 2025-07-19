712000980988e552213edd46a7492d823e259c8a
Darshak <darshakgoswami122@gmail.com>
2025-07-19 12:36:46 +0530

    added

--- String/First.java
+++ String/First.java
@@ -1,13 +1,25 @@
 package String;
 
+import java.util.Scanner;
+
 public class First
 {
 
 	public static void main(String[] args)
 	{
+		Scanner sc=new Scanner(System.in);
 		String s = " Hello World ";
 		String s1 = "Hello";
-		System.out.println(s.length());
+        //String email = "darshak122@gmail.com";
+        //int atIndex = email.indexOf('@');
+        //String username = email.substring(0, atIndex);
+        //System.out.println("Username: " + username);
+		//---------USER DEFINED EMAIL------------
+		System.out.println("enter email id :");
+        String email = sc.nextLine();
+        int atIndex = email.indexOf('@');
+        System.out.println("output is : "+email.substring(0, atIndex));
+        System.out.println(s.length());
 		System.out.println(s.indexOf('e'));
 		System.out.println(s.charAt(3));
 		System.out.println(s.equals(s1));
@@ -19,10 +31,11 @@ public class First
 		System.out.println(s.toLowerCase());
 		System.out.println(s.toUpperCase());
 		System.out.println(s.trim());
+		System.out.println(s.substring(0,3));
 		int i=10;
 		String s5=String.valueOf(i);
 		System.out.println(s5);
-		StringBuffer sb = new StringBuffer("hello");
+		StringBuffer sb = new StringBuffer("abcd");
 		System.out.println(sb.reverse());
 	}
 
